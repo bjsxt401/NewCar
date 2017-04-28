81d97c54144f59f03e19f587222c550f61c70aef
beichunming <beichunming@163.com>
2017-04-28 16:44:41 +0800

    add main,left,index,top  .jsp, controller method

--- src/main/java/cn/sxt/controller/DispatcherController.java
+++ src/main/java/cn/sxt/controller/DispatcherController.java
@@ -1,7 +1,17 @@
 package cn.sxt.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by beichunming on 2017/4/28.
  */
+@Controller
+@RequestMapping("/dispatcher")
 public class DispatcherController {
+    @RequestMapping("/{path}")
+    public String dispatcher(@PathVariable("path")String path){
+          return "commons/"+path;
+    }
 }
