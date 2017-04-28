e7c7d0eca9d1cb2a4ce2016107fbf5ab3e5f9fcd
beichunming <beichunming@163.com>
2017-04-28 15:01:09 +0800

    第一次提交 项目

---
+++ src/main/java/cn/sxt/entity/Users.java
@@ -0,0 +1,152 @@
+package cn.sxt.entity;
+
+import java.io.Serializable;
+
+/**
+ * Created by beichunming on 2017/4/28.
+ */
+public class Users implements Serializable{
+    private Integer uid;
+    private  String userName;
+    private  String identity;
+    private  String  fullname;
+    private  String  gender;
+    private String   address;
+    private String   phone;
+    private Integer  roleid;
+    private  String  userpwd;
+    private  String  position;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Users)) return false;
+
+        Users users = (Users) o;
+
+        if (getUid() != null ? !getUid().equals(users.getUid()) : users.getUid() != null) return false;
+        if (getUserName() != null ? !getUserName().equals(users.getUserName()) : users.getUserName() != null) return false;
+        if (getIdentity() != null ? !getIdentity().equals(users.getIdentity()) : users.getIdentity() != null) return false;
+        if (getFullname() != null ? !getFullname().equals(users.getFullname()) : users.getFullname() != null) return false;
+        if (getGender() != null ? !getGender().equals(users.getGender()) : users.getGender() != null) return false;
+        if (getAddress() != null ? !getAddress().equals(users.getAddress()) : users.getAddress() != null) return false;
+        if (getPhone() != null ? !getPhone().equals(users.getPhone()) : users.getPhone() != null) return false;
+        if (getRoleid() != null ? !getRoleid().equals(users.getRoleid()) : users.getRoleid() != null) return false;
+        if (getUserpwd() != null ? !getUserpwd().equals(users.getUserpwd()) : users.getUserpwd() != null) return false;
+        return getPosition() != null ? getPosition().equals(users.getPosition()) : users.getPosition() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getUid() != null ? getUid().hashCode() : 0;
+        result = 31 * result + (getUserName() != null ? getUserName().hashCode() : 0);
+        result = 31 * result + (getIdentity() != null ? getIdentity().hashCode() : 0);
+        result = 31 * result + (getFullname() != null ? getFullname().hashCode() : 0);
+        result = 31 * result + (getGender() != null ? getGender().hashCode() : 0);
+        result = 31 * result + (getAddress() != null ? getAddress().hashCode() : 0);
+        result = 31 * result + (getPhone() != null ? getPhone().hashCode() : 0);
+        result = 31 * result + (getRoleid() != null ? getRoleid().hashCode() : 0);
+        result = 31 * result + (getUserpwd() != null ? getUserpwd().hashCode() : 0);
+        result = 31 * result + (getPosition() != null ? getPosition().hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "uid=" + uid +
+                ", userName='" + userName + '\'' +
+                ", identity='" + identity + '\'' +
+                ", fullname='" + fullname + '\'' +
+                ", gender='" + gender + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                ", roleid=" + roleid +
+                ", userpwd='" + userpwd + '\'' +
+                ", position='" + position + '\'' +
+                '}';
+    }
+
+    public Users() {
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getIdentity() {
+        return identity;
+    }
+
+    public void setIdentity(String identity) {
+        this.identity = identity;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Integer getRoleid() {
+        return roleid;
+    }
+
+    public void setRoleid(Integer roleid) {
+        this.roleid = roleid;
+    }
+
+    public String getUserpwd() {
+        return userpwd;
+    }
+
+    public void setUserpwd(String userpwd) {
+        this.userpwd = userpwd;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+}
