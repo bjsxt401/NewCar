89d9bc00263d3dd262bd18f4742aa00a42815146
xuhanbo <294420720@qq.com>
2017-05-06 10:13:02 +0800

    完成出租单管理

--- src/main/java/cn/sxt/mapper/RentMapper.java
+++ src/main/java/cn/sxt/mapper/RentMapper.java
@@ -9,7 +9,14 @@ import java.util.Map;
  * Created by Administrator on 2017/5/4.
  */
 public interface RentMapper {
+    //创建订单
     void createRent(Rent rent);
+    //分页查询
     List<Rent> selectRentInfByCondition(Map<String,Object> map);
+    //查询总数
     Integer selectRentInfByConditionTotal(Rent rent);
+    //查询出租单信息
+    Rent selectRent(Rent rent);
+    //更新出租单单信息
+    void updateRent(Rent rent);
 }
